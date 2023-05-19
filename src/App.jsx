c787562dd43c6b2c73846b55955c473fb8b46db4
Carlos Diaz <carlosdiaz.dev@gmail.com>
2023-05-18 22:42:43 -0600

    Style changes and date for countdown

--- src/App.jsx
+++ src/App.jsx
@@ -7,7 +7,7 @@ function App() {
   return (
     <div className='flex items-center justify-center h-screen'>
         {/* <Counter date="2023-04-15T23:59:59"/> */}
-        <Counter date="2023-05-19T23:59:59"/>
+        <Counter date="2023-05-19T11:59:59"/>
       </div>
 
   )
--- src/components/Over.jsx
+++ src/components/Over.jsx
@@ -10,18 +10,19 @@ const Over = () => {
 
 
   return (
-    <div className='flex flex-col items-center justify-center text-center font-semibold h-screen'>
-      <p className='text-5xl mb-12'>"El tiempo ha valido la pena"</p>
-      <p className='text-4xl mb-12'>Te veo en México</p>
-      <p className='text-3xl mb-12'>😘 🇲🇽</p>
+    <div className='flex flex-col items-center justify-center h-screen font-semibold text-center'>
+      <p className='mb-12 text-5xl'>"El tiempo ha valido la pena"</p>
+      <p className='mb-12 text-5xl'>♥️    Te Amo    ♥️</p>
+      <p className='mb-12 text-4xl'>Te veo en México</p>
+      <p className='mb-12 text-5xl'>🇨🇴 😘 🇲🇽</p>
       <p className='text-3xl'>20 de Mayo 2023</p>
 
       <div class="flex items-center justify-center text-center mt-20">
         <div className="flex items-center justify-center text-center">
-          <button className="bg-green-500 flex items-center justify-center text-center hover:bg-green-600 transition duration-800 text-white py-3 px-4 rounded-lg" onClick={enviarMensaje}>
+          <button className="flex items-center justify-center px-4 py-3 text-center text-white transition bg-green-500 rounded-lg hover:bg-green-600 duration-800" onClick={enviarMensaje}>
 
             <FaWhatsapp size={36} />
-            <p className='text-lg ml-2'>Recordarle por WhatsApp</p>
+            <p className='ml-2 text-lg'>Recordarle por WhatsApp</p>
 
           </button>
         </div>
