6655b5e370d15f14d5e7aed3852ac5a53e6e7754
Carlos Diaz <carlosdiaz.dev@gmail.com>
2024-11-28 16:13:05 -0600

    fix: :bug: Update countdown date in Counter component

--- src/App.jsx
+++ src/App.jsx
@@ -8,7 +8,7 @@ function App() {
     <div className='flex items-center justify-center h-screen'>
         {/* <Counter date="2023-04-15T23:59:59"/> */}
         {/* <Counter date="2023-05-19T11:59:59"/> */}
-        <Counter date="2024-10-30T15:00:00"/>
+        <Counter date="2024-11-30T15:00:00"/>
       </div>
 
   )
